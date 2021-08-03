5b460355d988e8916973b36a4f4323625837312d
arshinx <arshinx@users.noreply.github.com>
2021-08-03 03:40:17 -0700

    Base Project Setup

---
+++ Leuna/Views/ScreenTwo.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenTwo.swift
+//  Leuna
+//
+//  Created by Arshin Jain on 8/3/21.
+//
+
+import SwiftUI
+
+struct ScreenTwo: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ScreenTwo_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenTwo()
+    }
+}
