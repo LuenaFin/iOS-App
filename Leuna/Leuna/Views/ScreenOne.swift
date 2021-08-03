784f309919056c9d09f57033d0b7a28d10a645a8
arshinx <arshinx@users.noreply.github.com>
2021-08-03 03:34:06 -0700

    Create ScreenOne.swift

---
+++ Leuna/Leuna/Views/ScreenOne.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenOne.swift
+//  Leuna
+//
+//  Created by Arshin Jain on 8/3/21.
+//
+
+import SwiftUI
+
+struct ScreenOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScreenOne_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenOne()
+    }
+}
