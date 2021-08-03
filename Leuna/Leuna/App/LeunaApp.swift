4594a720cedc65ca38bd028d1f6955ef2c0b5fad
arshinx <arshinx@users.noreply.github.com>
2021-08-03 03:34:10 -0700

    Create LeunaApp.swift

---
+++ Leuna/Leuna/App/LeunaApp.swift
@@ -0,0 +1,18 @@
+//
+//  LeunaApp.swift
+//  Leuna
+//
+//  Created by Arshin Jain on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LeunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            ScreenOne()
+        }
+    }
+}
