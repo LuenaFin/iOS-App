3c06f5765c6646a3ea85f670ad9f05fcf8cc78f8
Sophia Tan <sophiatanofficial@gmail.com>
2021-08-21 02:40:48 -0400

    Added Submit button for ScreenFour

--- Leuna/Views/ScreenFour.swift
+++ Leuna/Views/ScreenFour.swift
@@ -13,153 +13,168 @@ struct ScreenFour: View {
     var estimatedCost: Int { numShares * marketPrice }//Estimated cost based on numShares and marketPrice
     var body: some View {
                     
-        ScrollView(.vertical, showsIndicators: false, content: {
-            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
-                   spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
-                   content: {
-                VStack(
-                    alignment: .leading,
-                    spacing: 0,
-                    content: {
-                        HStack {
-                            Button(
-                                action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
-                                label: {
-                                    Text("Edit")
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(.green)
-                            })
-                                .padding(.vertical, 20)
-                        }
-                        
-                        VStack(
-                            alignment: .leading,
-                            spacing: nil,
-                            content: {
-                                Text("Buy AAPL")
-                                    .fontWeight(.medium)
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                                    .font(.system(size: 23))
-                                Text("$3,672.85 available")
-                                    .fontWeight(.medium)
-                                    .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
-                                    .font(.system(size: 15))
-                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                            }
-                        )
-                        .padding(.bottom, 40)
-                        
-                        //Number of Shares
-                        HStack(
-                            alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
-                            spacing: nil,
-                            content: {
-
-                                Text("Number of Shares")
-                                    .fontWeight(.medium)
-                                    .font(.system(size: 16))
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-
-                                Spacer()
-                                
-                                TextField("",
-                                          value: $numShares,
-                                          formatter: NumberFormatter())
-                                    .multilineTextAlignment(.trailing)
-                                    .frame(width: 60, height: 10, alignment: .trailing)
-                                    .foregroundColor(.white)
-                                    .keyboardType(UIKeyboardType.decimalPad)
-
-                        })
-                            .padding(.vertical, 20)
-
-                            .overlay(Rectangle()
-                                        .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
-                                        .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
-                        //Market Price
-                        HStack(
-                            alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
-                            spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
-                            content: {
-
-                                Text("Market Price")
-                                    .fontWeight(.medium)
-                                    .font(.system(size: 16))
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                                
-                                Image(systemName: "questionmark.circle")
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+               spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
+               content: {
+            VStack(
+                alignment: .leading,
+                spacing: 0,
+                content: {
+                    HStack {
+                        Button(
+                            action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+                            label: {
+                                Text("Edit")
+                                    .fontWeight(.semibold)
                                     .foregroundColor(.green)
-
-                                Spacer()
-                                
-                                TextField("$",
-                                          value: $marketPrice,
-                                          formatter: NumberFormatter())
-                                    .multilineTextAlignment(.trailing)
-                                    .frame(width: 60, height: 10, alignment: .trailing)
-                                    .foregroundColor(.white)
-                                    .keyboardType(UIKeyboardType.decimalPad)
-
                         })
                             .padding(.vertical, 20)
+                    }
+                    
+                    VStack(
+                        alignment: .leading,
+                        spacing: nil,
+                        content: {
+                            Text("Buy AAPL")
+                                .fontWeight(.medium)
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                                .font(.system(size: 23))
+                            Text("$3,672.85 available")
+                                .fontWeight(.medium)
+                                .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                                .font(.system(size: 15))
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        }
+                    )
+                    .padding(.bottom, 40)
+                    
+                    //Number of Shares
+                    HStack(
+                        alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                        spacing: nil,
+                        content: {
 
-                            .overlay(Rectangle()
-                                        .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
-                                        .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
-                        //Estimated Cost
-                        HStack(
-                            alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
-                            spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
-                            content: {
+                            Text("Number of Shares")
+                                .fontWeight(.medium)
+                                .font(.system(size: 16))
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
 
-                                Text("Estimated Cost")
-                                    .fontWeight(.medium)
-                                    .font(.system(size: 16))
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                            Spacer()
+                            
+                            TextField("",
+                                      value: $numShares,
+                                      formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: 60, height: 10, alignment: .trailing)
+                                .foregroundColor(.white)
+                                .keyboardType(UIKeyboardType.decimalPad)
 
-                                Spacer()
-                                
-                                TextField("",
-                                          value: $marketPrice,
-                                          formatter: NumberFormatter())
-                                    .multilineTextAlignment(.trailing)
-                                    .frame(width: 60, height: 10, alignment: .trailing)
-                                    .foregroundColor(.white)
-                                    .keyboardType(UIKeyboardType.decimalPad)
+                    })
+                        .padding(.vertical, 20)
 
-                        })
-                            .padding(.vertical, 20)
+                        .overlay(Rectangle()
+                                    .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
+                                    .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
+                    //Market Price
+                    HStack(
+                        alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                        spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
+                        content: {
 
-                            .overlay(Rectangle()
-                                        .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
-                                        .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
-                        
-                        
+                            Text("Market Price")
+                                .fontWeight(.medium)
+                                .font(.system(size: 16))
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                            
+                            Image(systemName: "questionmark.circle")
+                                .foregroundColor(.green)
+
+                            Spacer()
+                            
+                            TextField("$",
+                                      value: $marketPrice,
+                                      formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: 60, height: 10, alignment: .trailing)
+                                .foregroundColor(.white)
+                                .keyboardType(UIKeyboardType.decimalPad)
+
+                    })
+                        .padding(.vertical, 20)
+
+                        .overlay(Rectangle()
+                                    .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
+                                    .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
+                    //Estimated Cost
+                    HStack(
+                        alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                        spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
+                        content: {
+
+                            Text("Estimated Cost")
+                                .fontWeight(.medium)
+                                .font(.system(size: 16))
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+
+                            Spacer()
+                            
+                            TextField("",
+                                      value: $marketPrice,
+                                      formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: 60, height: 10, alignment: .trailing)
+                                .foregroundColor(.white)
+                                .keyboardType(UIKeyboardType.decimalPad)
+
+                    })
+                        .padding(.vertical, 20)
+
+                        .overlay(Rectangle()
+                                    .foregroundColor(Color(#colorLiteral(red: 0.1313615143, green: 0.1313910186, blue: 0.13135764, alpha: 0.9429324403)))
+                                    .frame(width: 390, height: 1.5, alignment: .top), alignment: .bottom)
+                    
+                    
+                    
+            })
+            Spacer()
+                
+            VStack(
+                alignment: .leading,
+                spacing: 5,
+                content: {
+                Text("Order Summary")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .font(.system(size: 15))
+                Text("You are placing a good for day market order to buy 1 share of AAPL. Your order will be placed after the market opens.")
+                    .foregroundColor(.white)
+                    .font(.system(size: 13))
+                    .padding(.bottom)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+                       label: {
+                        Text("Submit")
+                            .fontWeight(.semibold).foregroundColor(Color.white)
+                            .frame(width: 350, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(
+                                Capsule()
+                                    .foregroundColor(.green)
+                                    
+                        )
+                            .padding(.bottom)
                         
                 })
-                Spacer()
-                    
-                VStack(
-                    alignment: .leading,
-                    spacing: 5,
-                    content: {
-                    Text("Order Summary")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .font(.system(size: 15))
-                    Text("You are placing a good for day market order to buy 1 share of AAPL. Your order will be placed after the market opens.")
-                        .foregroundColor(.white)
-                        .font(.system(size: 13))
-                })
-                    
-                   
             })
                 
+            
+                
+               
         })
+            
+    
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .background(
             Color.black
